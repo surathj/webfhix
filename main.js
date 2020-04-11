6848371ac9568912d6b4cea22ba4224797bfb2d3
surathj <surathjayetileke@hotmail.com>
2020-04-11 16:44:29 +0200

    channel name test

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const channelForm = document.getElementById('channel-form');
 const channelInput = document.getElementById('channel-input');
 const videoContainer = document.getElementById('video-container');
 
-const defaultChannel = 'techguyweb';
+const defaultChannel = 'Gerard Jayetileke';
 
 // Form submit and change channel
 channelForm.addEventListener('submit', e => {
